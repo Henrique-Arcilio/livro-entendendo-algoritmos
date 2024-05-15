62b189770a0afe87d23e86845fe38a3ca9dbe51f
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2024-05-15 15:32:37 -0300

    Reformulado metodos de buscar menores e maiores
    Foi reformulada a forma de buscar os valores a serem ordenados, para
    conseguir ir "apagando" os que já foram encontrador colocando "null" no
    seu lugar. Para isso foi o tipo de array passou de int para Integer.

--- ordenacao-por-selecao/ordenacao/src/Ordenador.java
+++ ordenacao-por-selecao/ordenacao/src/Ordenador.java
@@ -1,24 +1,33 @@
 public class Ordenador {
 
-    public static int buscarMenor(int[] array){
+    public static int buscarMenor(Integer[] array){
             int menor = array[0];
-            for (int num : array){
-                if (num < menor){
-                    menor = num;
+            int indexDoMenor = 0;
+            for (int i = 0; i < (array.length - 1); i++){
+                if (array[i] < menor){
+                    menor = array[i];
+                    indexDoMenor = i;
                 }
             }
+            array[indexDoMenor] = null;
             return menor;
     }
 
-    public static int buscarMaior(int[] array){
+    public static int buscarMaior(Integer[] array){
         int maior = array[0];
-        for (int num : array){
-            if (num > maior){
-                maior = num;
+        int indexDoMaior = 0;
+        for (int i = 0; i < (array.length - 1); i++){
+            if (array[i] > maior){
+                maior = array[i];
+                indexDoMaior = i;
             }
         }
+        array[indexDoMaior] = null;
         return maior;
     }
 
-    //public static int ordenar(int[] array, int escolha){
-//}
+    //public static int ordenar(Integer[] array, int escolha){
+        //Integer[] arrayOrdenado = new Integer[array.length];
+
+    //}
+}
