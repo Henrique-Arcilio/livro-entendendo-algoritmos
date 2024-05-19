0f9d6635a480df8fff0521ace4bbb5968a38a2a5
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2024-05-19 13:06:46 -0300

    Implementação da base do algoritmo
    foi feita a base do algoritmo com um teste inicial manual no main.

---
+++ quicksort/src/QuickSort.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class QuickSort {
+    public static ArrayList<Integer> ordenar(ArrayList<Integer> array ){
+
+        ArrayList<Integer> arrayOrdenado = new ArrayList<>();
+        ArrayList<Integer> arrayMenores = new ArrayList<>();
+        ArrayList<Integer> arrayMaiores = new ArrayList<>();
+
+        if(array.size() < 2){
+            return array;
+        }
+        else{
+            int pivo = array.get(0);
+            for(int i = 1; i < array.size(); i++){
+                if(array.get(i) < pivo){
+                    arrayMenores.add(array.get(i));
+                }
+            }for (int i = 1; i < array.size(); i++){
+                if(array.get(i) > pivo){
+                    arrayMaiores.add(array.get(i));
+                }
+            }
+            arrayOrdenado.addAll(ordenar(arrayMenores));
+            arrayOrdenado.add(pivo);
+            arrayOrdenado.addAll(ordenar(arrayMaiores));
+            return arrayOrdenado;
+        }
+
+    }
+}
